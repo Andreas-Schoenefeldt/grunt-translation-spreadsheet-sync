33731c23674a5dc156a6392f2a5ac7f9b111e124
Andreas Schönefeldt <schoenefeldt.andreas@gmail.com>
2019-04-04 17:08:23 +0700

    Incorporation of the latest google-spreadsheet-translation-sync library

--- tasks/translation_spreadsheet_sync.js
+++ tasks/translation_spreadsheet_sync.js
@@ -8,43 +8,76 @@
 
 'use strict';
 
+const gstSync = require('google-spreadsheet-translation-sync');
+const path = require('path');
+
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('translation_spreadsheet_sync', 'A grunt plugin to manage translation import/export to and from a google spreadsheet', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    grunt.registerMultiTask('translation_spreadsheet_sync', 'A grunt plugin to manage translation import/export to and from a google spreadsheet', function() {
+        const done = this.async();
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+        // Merge task-specific and/or target-specific options with these defaults.
+        const options = this.options({
+            keyId: 'key',
+            credentials: require('google-spreadsheet-translation-sync/test/data/google-test-access.json'),
+            translationFormat: 'locale_json'
+        });
+
+        if (! options.spreadsheetId) {
+            grunt.fatal('The option spreadsheetId must be defined!');
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        if (! options.mode) {
+            grunt.fatal('The option mode must be defined!');
+        }
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        let translationFiles = [];
+        let rootFolder = null;
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+        this.files.forEach( function (f) {
+            const files = f.src.filter(function(filepath) {
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else if (options.mode) {
+                    let base;
+
+                    if (grunt.file.isFile(filepath)) {
+                        base = path.basename(filepath);
+                    } else if (grunt.file.isDir(filepath)) {
+                        base = filepath;
+                    }
+
+                    if (!rootFolder || rootFolder === base) {
+                        rootFolder = base;
+                    } else {
+                        grunt.log.warn('Your file configuration mathes more then one root folder, we stick to  "' + rootFolder + ' (ignored ' + base + ')');
+                    }
+                }
+                return true;
+            });
+
+            translationFiles = translationFiles.concat(files);
+        });
+
+        switch (options.mode) {
+            default:
+                grunt.fatal('Unrecognised mode option value ' + options.mode);
+                break;
+            case 'upload':
+                gstSync.exportToSpreadsheet(translationFiles, options.spreadsheetId, options.credentials, options.translationFormat, function () {
+                    done();
+                });
+                break;
+            case 'import':
+                gstSync.exportToSpreadsheet(rootFolder, options.spreadsheetId, options.credentials, options.translationFormat, function () {
+                    done();
+                });
+                break;
+        }
     });
-  });
 
 };
